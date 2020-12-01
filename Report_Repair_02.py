11916f93c0b6ebd7f70f30d00bd051489a78f161
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2020-12-01 17:06:52 -0600

    part2 day one

---
+++ Report_Repair_02.py
@@ -0,0 +1,20 @@
+with open('input.txt', 'r') as f:
+    data = [int(i) for i in f.read().split()]
+
+data.sort()
+
+print(data)
+
+answer = 0
+index = 0
+
+for i in data:
+    index += 1
+    data2 = data[index:]
+    data3 = data[index+1:]
+    for j in data2:
+        for k in data3:
+            if i+j+k == 2020:
+                print(i,j,k)
+                answer = i*j*k
+print(answer)
