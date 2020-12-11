78bd06c54ee7fc38214158070d3e20ae0e8a5b98
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2020-12-11 16:43:19 -0600

    final day 9 commit

--- Day9/Encoding_Error.py
+++ Day9/Encoding_Error.py
@@ -2,11 +2,13 @@
 --- Day 9: Encoding Error ---
 With your neighbor happily enjoying their video game, you turn your attention to an open data port on the little screen in the seat in front of you.
 
-Though the port is non-standard, you manage to connect it to your computer through the clever use of several paperclips. Upon connection, the port outputs a series of numbers (your puzzle input).
+Though the port is non-standard, you manage to connect it to your computer through the clever use of several paperclips. Upon connection, the port 
+outputs a series of numbers (your puzzle input).
 
 The data appears to be encrypted with the eXchange-Masking Addition System (XMAS) which, conveniently for you, is an old cypher with an important weakness.
 
-XMAS starts by transmitting a preamble of 25 numbers. After that, each number you receive should be the sum of any two of the 25 immediately previous numbers. The two numbers will have different values, and there might be more than one such pair.
+XMAS starts by transmitting a preamble of 25 numbers. After that, each number you receive should be the sum of any two of the 25 immediately previous numbers. 
+The two numbers will have different values, and there might be more than one such pair.
 
 For example, suppose your preamble consists of the numbers 1 through 25 in a random order. To be valid, the next number must be the sum of two of those numbers:
 
@@ -14,7 +16,8 @@ For example, suppose your preamble consists of the numbers 1 through 25 in a ran
 49 would be a valid next number, as it is the sum of 24 and 25.
 100 would not be valid; no two of the previous 25 numbers sum to 100.
 50 would also not be valid; although 25 appears in the previous 25 numbers, the two numbers in the pair must be different.
-Suppose the 26th number is 45, and the first number (no longer an option, as it is more than 25 numbers ago) was 20. Now, for the next number to be valid, there needs to be some pair of numbers among 1-19, 21-25, or 45 that add up to it:
+Suppose the 26th number is 45, and the first number (no longer an option, as it is more than 25 numbers ago) was 20. Now, for the next number to be valid, 
+there needs to be some pair of numbers among 1-19, 21-25, or 45 that add up to it:
 
 26 would still be a valid next number, as 1 and 25 are still within the previous 25 numbers.
 65 would not be valid, as no two of the available numbers sum to it.
@@ -43,14 +46,16 @@ Here is a larger example which only considers the previous 5 numbers (and has a 
 576
 In this example, after the 5-number preamble, almost every number is the sum of two of the previous 5 numbers; the only number that does not follow this rule is 127.
 
-The first step of attacking the weakness in the XMAS data is to find the first number in the list (after the preamble) which is not the sum of two of the 25 numbers before it. What is the first number that does not have this property?
+The first step of attacking the weakness in the XMAS data is to find the first number in the list (after the preamble) which is not the sum of two of the 25 numbers 
+before it. What is the first number that does not have this property?
 
 Your puzzle answer was 1398413738.
 
 The first half of this puzzle is complete! It provides one gold star: *
 
 --- Part Two ---
-The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a contiguous set of at least two numbers in your list which sum to the invalid number from step 1.
+The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a contiguous set of at least two numbers in your list which 
+sum to the invalid number from step 1.
 
 Again consider the above example:
 
@@ -74,7 +79,8 @@ Again consider the above example:
 277
 309
 576
-In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course, the contiguous set of numbers in your actual list might be much longer.)
+In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course, the contiguous set of numbers in your actual 
+list might be much longer.)
 
 To find the encryption weakness, add together the smallest and largest number in this contiguous range; in this example, these are 15 and 47, producing 62.
 
@@ -104,8 +110,20 @@ def search(lst, preamble_length):
         else:
             return k
 
+def binary_search(lst, val):   
+    mini = 0
+    maxi = 1
 
+    for i in range(len(lst) - 1):        
 
+        if sum(lst[mini:maxi]) < val:
+            maxi += 1
+
+        while sum(lst[mini:maxi]) > val:
+            mini += 1
+
+        if sum(lst[mini:maxi]) == val:
+            return (min(lst[mini:maxi]) + max(lst[mini:maxi]))
 
 
 if __name__ == "__main__":
@@ -117,7 +135,9 @@ if __name__ == "__main__":
 
         test_data = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576]
 
-    print('Invalid number: {0}'.format(search(data, 25)))
+    # print('Invalid number: {0}'.format(search(data, 25)))
+    print('Total of min/max: {0}'.format(binary_search(data, 1398413738)))
+    # print('Total of min/max: {0}'.format(binary_search(test_data, 127)))
 
    
 
---
+++ Day9/test.py
@@ -0,0 +1,5 @@
+lst = [1,5,9,26,13,20,156]
+x = sum(lst)
+print(x)
+
+print(max(lst), min(lst))
