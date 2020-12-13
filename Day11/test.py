3434cf607c564e5815e9af1e50ccc0362d0696c8
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2020-12-13 14:43:07 -0600

    Day 11 Final, need cleanup

--- Day11/test.py
+++ Day11/test.py
@@ -4,3 +4,19 @@ copy_of_test = test.copy()
 test[0] = 15
 
 print(test, copy_of_test)
+
+
+
+
+def check_adjacency(y_coor, x_coor, adj_dict):
+    adj_pointers = [( -1, -1 ), ( 0, -1 ), ( 1, -1 ), ( -1, 0 ), ( 1, 0 ), ( -1, 1 ), ( 0, 1 ), ( 1, 1 ) ]
+    adj_counter = 0
+
+    for i in adj_pointers:
+        key_to_lookup = (y_coor + i[0], x_coor + i[1])
+        if key_to_lookup in adj_dict:
+            adj_counter += adj_dict[(y_coor + i[0], x_coor + i[1])]
+        else:
+            continue
+
+    return adj_counter
