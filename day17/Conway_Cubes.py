13bb4b1aa1c186f75fa7ff7ffeb694cc04ce993a
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2020-12-22 23:52:29 -0600

    Day 17 pt1 fin

--- day17/Conway_Cubes.py
+++ day17/Conway_Cubes.py
@@ -150,10 +150,17 @@ class Cube:
         self.z = z
         self.state = state
 
+    def __str__(self):
+        return 'Cube class with {} state'.format(self.state)
+
+    def __repr__(self):
+        return 'Cube({},{},{},{})'.format(self.y, self.x, self.z, self.state)
 
     def change_state(self):
         self.state = not self.state
-        return self.state
+        # return self.state
+
+
 
 
 
@@ -162,6 +169,26 @@ class Space:
     def __init__(self):
         self.dict = {}
         self.old_dict = {}
+        self.transition = []
+
+        self.search_area = [ (-1,-1,-1), (0,-1,-1), (1,-1,-1),
+                             (-1, 0,-1), (0, 0,-1), (1, 0,-1), # Relative Z - 1 
+                             (-1, 1,-1), (0, 1,-1), (1, 1,-1),
+
+                             (-1,-1, 0), (0,-1, 0), (1,-1, 0),
+                             (-1, 0, 0),            (1, 0, 0), # Relative Z = 0
+                             (-1, 1, 0), (0, 1, 0), (1, 1, 0),
+                       
+                             (-1,-1, 1), (0,-1, 1), (1,-1, 1),
+                             (-1, 0, 1), (0, 0, 1), (1, 0, 1), # Relative Z + 1
+                             (-1, 1, 1), (0, 1, 1), (1, 1, 1),
+                            ]
+
+    def __str__(self):
+        return 'Space class'
+
+    def __repr__(self):
+        return 'Space()'
 
 
     def create_adj_cubes(self):
@@ -170,8 +197,8 @@ class Space:
 
     def ini_cube(self, y, x, z, state):
         key = self.coord_to_key(y, x, z)
-        newCube = Cube(y, x, z)
-        self.dict[key] = newCube.state
+        newCube = Cube(y, x, z, state)
+        self.dict[key] = newCube
 
 
     def coord_to_key(self, y, x, z):
@@ -180,58 +207,69 @@ class Space:
 
 
     def adj_search(self, key_val):
-
-        search_area = [ (-1, 1, 1), (0, 1, 1), (1, 1, 1),
-                        (-1, 0, 1), (0, 0, 1), (1, 0, 1), # Relative Z + 1 
-                        (-1,-1, 1), (0,-1, 1), (1,-1, 1),
-
-                        (-1, 1, 0), (0, 1, 0), (1, 1, 0),
-                        (-1, 0, 0),            (1, 0, 0), # Relative Z = 0
-                        (-1,-1, 0), (0,-1, 0), (1,-1, 0),
-
-                        (-1, 1,-1), (0, 1,-1), (1, 1,-1),
-                        (-1, 0,-1), (0, 0,-1), (1, 0,-1), # Relative Z - 1
-                        (-1,-1,-1), (0,-1,-1), (1,-1,-1),
-                      ]
-
-        active_count = 0
-
-        for i in search_area:
-            y = key_val.y + i[0]
-            x = key_val.x + i[1]
-            z = key_val.z + i[2]
+        
+        for i in self.search_area:
+            y = self.dict[key_val].y + i[0]
+            x = self.dict[key_val].x + i[1]
+            z = self.dict[key_val].z + i[2]
 
             key = self.coord_to_key(y,x,z)
 
-        if key not in self.old_dict:
+            if key not in self.old_dict:
 
-            newCube = Cube(y, x, z)
-            self.dict[key] = newCube.state
+                newCube = Cube(y, x, z)
+                self.dict[key] = newCube
 
-        else:
+    def check_cubes(self, key_val):
 
-            if self.dict[key] == True:
-                active_count += 1
+        active_count = 0
 
-        return active_count
+        for i in self.search_area:
+            y = self.dict[key_val].y + i[0]
+            x = self.dict[key_val].x + i[1]
+            z = self.dict[key_val].z + i[2]
+
+            key = self.coord_to_key(y,x,z)
+
+            if key not in self.dict:
+                pass
+            
+            else:
+
+                if self.old_dict[key].state == True:
+                    active_count += 1
+        
+        self.cube_eval_state(key_val, active_count)
+
+
+
+       
 
 
     def cube_eval_state(self, key_val, active_count):
 
-        if key_val.state == True and (active_count < 2 or active_count > 3):
-            key_val.state = key_val.change_state()
-            self.dict[key_val] = key_val.state
-        elif key_val.state == False and active_count == 3:
-            key_val.state = key_val.change_state()
-            self.dict[key_val] = key_val.state
+        if self.old_dict[key_val].state == True and (active_count < 2 or active_count > 3):
+            self.transition.append(key_val)
+            # self.dict[key_val].change_state()
+            # self.dict[key_val] = self.dict[key_val].state
+        elif self.old_dict[key_val].state == False and active_count == 3:
+            self.transition.append(key_val)
+            # self.dict[key_val].change_state()
+            # self.dict[key_val] = self.dict[key_val].state
             
 
     def start_cycle(self):
         self.old_dict = self.dict.copy()
-        
+
         for i in self.old_dict.keys():
-            active_count = self.adj_search(i)
-            self.cube_eval_state(i, active_count)
+            if self.old_dict[i].state is True:
+                self.adj_search(i)
+        
+        self.old_dict = self.dict.copy()
+
+        for i in self.old_dict.keys():
+            self.check_cubes(i)
+
 
 
 
@@ -246,7 +284,7 @@ def load_map(data, uni):
             elif data[row][column] == '#':
                 uni.ini_cube(row, column, z, state = True)    
 
-                
+
 
 def tesseract(data, cycle):
     mcu = Space()
@@ -255,9 +293,14 @@ def tesseract(data, cycle):
 
     for i in range(cycle):
         mcu.start_cycle()
+  
+        for i in mcu.transition:
+            mcu.dict[i].change_state()
+        
+        mcu.transition.clear()
         
     for i in mcu.dict.values():
-        if i == True:
+        if i.state == True:
             cube_active += 1
     
     return cube_active, cycle
--- test.py
+++ test.py
@@ -1,40 +1,143 @@
-# y = 0
-# x = 1
-# z = -1
 
-# key = '(' + str(y) + ',' + str(x) + ',' + str(z) + ')'
-# print(key)
+class Cube:
 
-# dict = {}
+    def __init__(self, y, x, z, state = False):
+        self.y = y
+        self.x = x
+        self.z = z
+        self.state = state
 
-# dict[key] = 23
+    def __str__(self):
+        return 'Cube class with {} state'.format(self.state)
 
-# g = dict['(0,1,-1)']
+    def __repr__(self):
+        return 'Cube({},{},{},{})'.format(self.y, self.x, self.z, self.state)
 
-# print(g)
-
-
-# state = True
-# x = 5
-
-# if state == True and (x < 2 or x > 4):
-#     state = False
+    def change_state(self):
+        self.state = not self.state
+        return self.state
 
 
-# print(state)
 
-search_area = [(-1, 1, 1), (0, 1, 1), (1, 1, 1),
-               (-1, 0, 1), (0, 0, 1), (1, 0, 1), # Relative Z + 1 
-               (-1,-1, 1), (0,-1, 1), (1,-1, 1),
 
-               (-1, 1, 0), (0, 1, 0), (1, 1, 0),
-               (-1, 0, 0)       ,     (1, 0, 0), # Relative Z = 0
-               (-1,-1, 0), (0,-1, 0), (1,-1, 0),
 
-               (-1, 1,-1), (0, 1,-1), (1, 1,-1),
-               (-1, 0,-1), (0, 0,-1), (1, 0,-1), # Relative Z - 1
-               (-1,-1,-1), (0,-1,-1), (1,-1,-1),
-               ]
+class Space:
 
-for i in search_area:
-    print(i[0], i[1], i[2])
+    def __init__(self):
+        self.dict = {}
+        self.old_dict = {}
+
+    def __str__(self):
+        return 'Space class'
+
+    def __repr__(self):
+        return 'Space()'
+
+    def ini_cube(self, y, x, z, state):
+        key = self.coord_to_key(y, x, z)
+        newCube = Cube(y, x, z, state)
+        self.dict[key] = newCube
+
+
+    def coord_to_key(self, y, x, z):
+        key = '(' + str(y) + ',' + str(x) + ',' + str(z) + ')'
+        return key
+
+
+    def adj_search(self, key_val):
+
+        search_area = [ (-1,-1,-1), (0,-1,-1), (1,-1,-1),
+                        (-1, 0,-1), (0, 0,-1), (1, 0,-1), # Relative Z - 1 
+                        (-1, 1,-1), (0, 1,-1), (1, 1,-1),
+
+                        (-1,-1, 0), (0,-1, 0), (1,-1, 0),
+                        (-1, 0, 0), (0, 0, 0), (1, 0, 0), # Relative Z = 0
+                        (-1, 1, 0), (0, 1, 0), (1, 1, 0),
+                       
+                        (-1,-1, 1), (0,-1, 1), (1,-1, 1),
+                        (-1, 0, 1), (0, 0, 1), (1, 0, 1), # Relative Z + 1
+                        (-1, 1, 1), (0, 1, 1), (1, 1, 1),
+                      ]
+
+        active_count = 0
+
+        for i in search_area:
+            y = self.dict[key_val].y + i[0]
+            x = self.dict[key_val].x + i[1]
+            z = self.dict[key_val].z + i[2]
+
+            key = self.coord_to_key(y,x,z)
+
+            if key not in self.old_dict:
+
+                newCube = Cube(y, x, z)
+                self.dict[key] = newCube
+
+            else:
+
+                if self.dict[key] == True:
+                    active_count += 1
+
+        return active_count
+
+
+    def cube_eval_state(self, key_val, active_count):
+
+        if self.dict[key_val].state == True and (active_count < 2 or active_count > 3):
+            self.dict[key_val].state = self.dict[key_val].change_state()
+            # self.dict[key_val] = self.dict[key_val].state
+        elif self.dict[key_val].state == False and active_count == 3:
+            self.dict[key_val].state = self.dict[key_val].change_state()
+            # self.dict[key_val] = self.dict[key_val].state
+            
+
+    def start_cycle(self):
+        self.old_dict = self.dict.copy()
+        
+        for i in self.old_dict.keys():
+            active_count = self.adj_search(i)
+            self.cube_eval_state(i, active_count)
+
+
+
+
+def load_map(data, uni):
+    z = 0
+    
+    for row in range(len(data)):
+        for column in range(len(data[0])):
+            if data[row][column] == '.':
+                uni.ini_cube(row, column, z, state = False)
+            elif data[row][column] == '#':
+                uni.ini_cube(row, column, z, state = True)    
+
+
+
+def tesseract(data, cycle):
+    mcu = Space()
+    load_map(data, mcu)
+    cube_active = 0
+
+    for i in range(cycle):
+        mcu.start_cycle()
+        
+    for i in mcu.dict.values():
+        if i.state == True:
+            # print(i)
+            cube_active += 1
+    
+    return cube_active, cycle
+
+###------------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    input = r'C:\Users\alanv\PythonCode\Projects\Advent of Code 2020\day17\input.txt'
+
+    with open(input, 'r') as f:
+        data = f.read().split('\n')
+        cycle_length = 6
+        
+
+    a, b = tesseract(data, cycle_length)
+
+    print('{0} Cubes in active state at end of {1} cycle boot process'. format(a, b))
