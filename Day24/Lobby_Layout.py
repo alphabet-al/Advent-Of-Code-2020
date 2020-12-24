281bdb8944cc16fdacd1c36abf07aa14ab30ea94
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2020-12-24 16:46:36 -0600

    Day 24 pt1 done

--- Day24/Lobby_Layout.py
+++ Day24/Lobby_Layout.py
@@ -55,46 +55,42 @@ def lay_tile(data):
     count = 0
 
     for i in split_data:
-        # print(i)
+
         current_pos = lobby.upd_pos(0,0,0)
         do_instruction(i, lobby, current_pos)
-        # for i in lobby.floor_map.values():
-        #     if i.state is False:
-        #         print(i)
-
 
     for i in lobby.floor_map.values():
         if i.state is False:
             count += 1
 
-    print(count)
+    return count
 
 def do_instruction(line, lob, current_pos):
  
     for i in line:
 
         if i == 'e':
-            current_pos = lob.upd_pos(current_pos.x + 1, current_pos.y, current_pos.z)
+            current_pos = lob.upd_pos(current_pos.x + 1, current_pos.y - 1, current_pos.z)
         elif i == 'w':
-            current_pos = lob.upd_pos(current_pos.x - 1, current_pos.y, current_pos.z)
+            current_pos = lob.upd_pos(current_pos.x - 1, current_pos.y + 1, current_pos.z)
         elif i == 'ne':
-            current_pos = lob.upd_pos(current_pos.x, current_pos.y, current_pos.z - 1)
+            current_pos = lob.upd_pos(current_pos.x + 1, current_pos.y, current_pos.z - 1)
         elif i == 'sw':
-            current_pos = lob.upd_pos(current_pos.x, current_pos.y, current_pos.z + 1)  
+            current_pos = lob.upd_pos(current_pos.x - 1, current_pos.y, current_pos.z + 1)  
         elif i == 'nw':
-            current_pos = lob.upd_pos(current_pos.x, current_pos.y + 1, current_pos.z)
+            current_pos = lob.upd_pos(current_pos.x, current_pos.y + 1, current_pos.z - 1)
         elif i == 'se':
-            current_pos = lob.upd_pos(current_pos.x, current_pos.y - 1, current_pos.z) 
+            current_pos = lob.upd_pos(current_pos.x, current_pos.y - 1, current_pos.z + 1) 
 
     current_pos.change_state()
 
 
 if __name__ == "__main__":
-    input = r"C:\Users\alanv\PythonCode\Projects\Advent of Code 2020\Day24\t_input.txt"
+    input = r"C:\Users\alanv\PythonCode\Projects\Advent of Code 2020\Day24\input.txt"
 
     with open(input, 'r') as f:
         data = f.read().split()
 
         # print(data)
 
-        lay_tile(data)
+        print('{} tiles with black side up'.format(lay_tile(data)))
